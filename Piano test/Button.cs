4e9bb0424e4f4d4641340d008f5b92cc52da6bc6
dt4 <134435705+dt4256@users.noreply.github.com>
2026-01-14 22:56:57 +0300

    init first try
    тут проще заклонить и посмотреть

---
+++ Piano test/Button.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Piano_test
+{
+    class BTN
+    {
+        private int hight,pitchband,pressure;
+        private int config;//просто про расположение черных клавиш рядом(заморочка с pitch band) где 0 слева справа есть черные  клавиши(pitch band не резать) -1  черная клавиша только слева 1 черная клавиша только справа
+        private char note;
+        private bool pressured;
+        private int x, y;
+        private int power = 127;
+        private int width, height;
+
+        public bool Pressured
+        {
+            get { return pressured; }
+            set { pressured = value; }
+        }
+        public int Width
+        {
+            get { return width; }
+        }
+        public char Note
+        {
+            get { return note; }
+        }
+        public int Pitchband
+        {
+            get { return pitchband; }
+        }
+        public BTN(int x,int y, int hight, int config, char note)
+        {
+            this.x= x; this.y = y;
+            this.config = config;
+            this.hight = hight;
+            this.note = note;
+            pitchband = 8192;
+            pressure = 0;
+            width = 100;
+            height = 200;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen clr = new Pen(Color.Black);
+            g.DrawLine(clr, x, y, x + width, y);
+            g.DrawLine(clr, x, y, x, y + height);
+            g.DrawLine(clr, x + width, y, x + width, y + height);
+            if (config == 0) g.DrawLine(clr, x, y + height, x + width, y + height);
+            if (config == -1) g.DrawLine(new Pen(Color.Blue), x, y + height, x + width, y + height);
+            if (config == 1) g.DrawLine(new Pen(Color.Red), x, y + height, x + width, y + height);
+        }
+
+        public bool isInside(int x, int y) {
+            if(x>=this.x && x <= this.x + width)
+            {
+                if(y>=this.y && y <= this.y + height)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private int Map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+        {
+             return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+        public void change_pitchband(int x)
+        {
+            pitchband = Map(x-this.x, 0, width, 0, 16383);
+        }
+        
+    }
+}
---
+++ Piano test/Form1.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Piano_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<BTN> btns = new List<BTN>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int tonestart = 2;
+            int xst = 100, yst = 100, dist = 20;
+            string notes = "CDEFGAB";
+            int[] statuses = { 1, 0, -1, 1, 0, 0, -1 };//главное начинать с до инициализацию
+            for (int i = 0; i < 15; i++)
+            {
+                btns.Add(new BTN(xst, yst, tonestart + i, statuses[i % 7], notes[i % 7]));
+                xst += btns[i].Width;
+                xst += dist;
+            }
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < btns.Count; i++)
+            {
+                btns[i].Draw(e.Graphics);
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < btns.Count; i++)
+            {
+                if (btns[i].isInside(e.X, e.Y))
+                {
+                    btns[i].change_pitchband(e.X);
+                    btns[i].Pressured = true;
+                    label1.Text = Convert.ToString(i) + btns[i].Note + Convert.ToString(btns[i].Pitchband);
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < btns.Count; i++)
+            {
+                if (btns[i].isInside(e.X, e.Y) && btns[i].Pressured)
+                {
+                    btns[i].change_pitchband(e.X);
+                    label1.Text = Convert.ToString(i) + btns[i].Note + Convert.ToString(btns[i].Pitchband);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            for(int i = 0; i < btns.Count; i++)
+            {
+                btns[i].Pressured = false;
+            }
+        }
+
+
+
+       
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            for(int i = 0;i< btns.Count; i++)
+            {
+                if (btns[i].Pressured) btns[i].Pressured = false;
+            }
+        }
+    }
+}
